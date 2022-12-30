35d1095212ac5729d7deebea1a4a53e69d1541e1
mutsune <dev@mutsune.com>
2022-12-30 22:09:09 +0900

    prolog day1 selfstudy

---
+++ prolog/day1/selfstudy/book.pl
@@ -0,0 +1,6 @@
+book(b1).
+book(b2).
+author(a1, b1).
+author(a1, b2).
+
+% author(a1, Y).
---
+++ prolog/day1/selfstudy/music.pl
@@ -0,0 +1,6 @@
+artist(a1, piano).
+artist(a2, guiter).
+genre(a1, classic).
+genre(a1, rock).
+
+% artist(X, guiter).
